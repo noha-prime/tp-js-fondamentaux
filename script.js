0f3d79ccacf6b900f38b02ed3a234472cfa5139a
noha-prime <noha.dairi@gmail.com>
2025-11-04 14:57:50 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 console.log ("Laboratoire prêt !")
+const nom = "Dairi";
+let age = 16;
 
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "12345"; // Changez cette valeur pour tester
 
+if ("Comparaison entre motDePasseUtilisateur et motDePasseAttendu") {
+    console.log("Accès autorisé. Bienvenue !");
+} else if ("Vérification de la longueur du mot de passe (< 8 caractères)") {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
