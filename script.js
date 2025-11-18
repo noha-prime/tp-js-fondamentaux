478d0b222d602308769768e0b66357ba6f295b3d
noha-prime <noha.dairi@gmail.com>
2025-11-18 13:28:52 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ let age = 16;
 const motDePasseAttendu = "secret123";
 let motDePasseUtilisateur = "53214"; // Changez cette valeur pour tester
 
-if ("Comparaison entre motDePasseUtilisateur et motDePasseAttendu") {
+if ("Comparaison entre motDePasseUtilisateur et motDePasseAttendu") { 
     console.log("Accès autorisé. Bienvenue !");
 } else if ("Vérification de la longueur du mot de passe (< 8 caractères)") {
     console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
 } else {
     console.log("Accès refusé. Mot de passe incorrect.");
-}
+    const moi = {
+    prenom: "VotrePrénom",
+    nom: "VotreNom",
+    age: 18,
+    competences: ["HTML", "CSS"]
+};
+console.log(moi.prenom); // Affiche "VotrePrénom"
+// Affiche je m'apelle Noha Dairi 
+ moi.age = 19;
+    moi.ville = "Paris"; 
+} 
+
